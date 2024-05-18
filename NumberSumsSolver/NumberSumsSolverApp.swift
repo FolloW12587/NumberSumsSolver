b95cbf9f4a6083835c3d2e9ffdf327b76bd6dc93
follow <follow12587@gmail.com>
2024-05-19 08:45:15 +1000

    Initial Commit

---
+++ NumberSumsSolver/NumberSumsSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberSumsSolverApp.swift
+//  NumberSumsSolver
+//
+//  Created by Сергей Дубовой on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NumberSumsSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
